b13ee76d4d67a1b739f7a4394547eb850b454d0d
Gabriel Tavares <tavaresgabriel963@gmail.com>
2021-05-17 16:42:29 -0400

    Category resource

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dscatalog.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dscatalog.entities.Category;
+
+//controla o controlador REST
+//recurso da classe Category
+
+@RestController
+@RequestMapping(value = "/categories") //rota
+public class CategoryResource {//TERMO
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll(){
+		List<Category> list = new ArrayList<>();
+		list.add(new Category(1L, "Books"));
+		list.add(new Category(2L, "Electonics"));
+		return ResponseEntity.ok().body(list);
+	}
+}
