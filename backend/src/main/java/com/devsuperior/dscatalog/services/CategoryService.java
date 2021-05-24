8c5eb95c6140cdbb400fc531d5ef2089383b92ea
Gabriel Tavares <tavaresgabriel963@gmail.com>
2021-05-24 14:48:35 -0400

    POST new Category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -32,4 +32,12 @@ public class CategoryService {
 		return new CategoryDTO(entity);
 	}
 
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+	}
+
 }
