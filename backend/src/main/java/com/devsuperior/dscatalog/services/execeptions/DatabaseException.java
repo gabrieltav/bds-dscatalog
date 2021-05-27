4ed5948169bbfabddf6b675790992835f9d47ea1
Gabriel Tavares <tavaresgabriel963@gmail.com>
2021-05-26 20:59:21 -0400

    Auditing data

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/execeptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dscatalog.services.execeptions;
+
+public class DatabaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+}
