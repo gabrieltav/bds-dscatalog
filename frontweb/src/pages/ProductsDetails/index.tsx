41107f05830c4fc674aaf17586510b39cde53d33
Gabriel Tavares <tavaresgabriel963@gmail.com>
2021-09-12 16:20:09 -0400

    useState, useEffect, useParams

--- frontweb/src/pages/ProductsDetails/index.tsx
+++ frontweb/src/pages/ProductsDetails/index.tsx
@@ -1,10 +1,28 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import axios from 'axios';
 import ProductPrice from 'components/ProductPrice';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Product } from 'types/products';
+import { BASE_URL } from 'util/requests';
 
 import './styles.css';
 
+type UrlParams = {
+  productId: string;
+};
+
 const ProductDetails = () => {
+  const { productId } = useParams<UrlParams>();
+
+  const [product, setProduct] = useState<Product>();
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/products/${productId}`).then((reponse) => {
+      setProduct(reponse.data);
+    });
+  }, [productId]);
+
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
@@ -17,23 +35,17 @@ const ProductDetails = () => {
         <div className="row">
           <div className="col-xl-6">
             <div className="img-container">
-              <img
-                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
-                alt="Nome do produto"
-              />
+              <img src={product?.imgUrl} alt={product?.name} />
             </div>
             <div className="name-price-container">
-              <h1>Nome do produto</h1>
-              <ProductPrice price={2345.67} />
+              <h1>{product?.name}</h1>
+              {product && <ProductPrice price={product?.price} />}
             </div>
           </div>
           <div className="col-xl-6">
             <div className="description-container">
               <h2>Descrição do produto</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quaerat, quas!
-              </p>
+              <p>{product?.description}</p>
             </div>
           </div>
         </div>
