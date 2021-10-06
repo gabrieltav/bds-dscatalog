4b1d34a5637fdf13b84f4c9b7e8ba0ede21aed7b
Gabriel Tavares <tavaresgabriel963@gmail.com>
2021-10-06 09:52:27 -0400

    Login card styling

--- frontweb/src/pages/Admin/Auth/index.tsx
+++ frontweb/src/pages/Admin/Auth/index.tsx
@@ -1,5 +1,6 @@
 import { ReactComponent as AuthImage } from 'assets/images/auth-image.svg';
 import { Route, Switch } from 'react-router-dom';
+import Login from './Login';
 
 import './styles.css';
 
@@ -17,7 +18,7 @@ const Auth = () => {
       <div className="auth-form-container">
         <Switch>
           <Route path="/admin/auth/login">
-            <h1>Card de Login</h1>
+            <Login />
           </Route>
           <Route path="/admin/auth/signup">
             <h1>Card de Signup</h1>
