cf87eb541f91cd59b1e47185af462a6ece150dc7
Gabriel Tavares <tavaresgabriel963@gmail.com>
2021-10-26 08:45:37 -0400

    Manual error handling

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -2,64 +2,75 @@ import { Link } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from 'react-hook-form';
 import { requestBackendLogin } from 'util/requests';
+import { useState } from 'react';
 
 import './styles.css';
 
 type FormData = {
-  username: string;
-  password: string;
+    username: string;
+    password: string;
 };
 
 const Login = () => {
-  const { register, handleSubmit } = useForm<FormData>();
 
-  const onSubmit = (formData: FormData) => {
-    requestBackendLogin(formData)
-      .then((response) => {
-        console.log('SUCESSO', response);
-      })
-      .catch((error) => {
-        console.log('ERRO', error);
-      });
-  };
+    const [hasError, setHasError] = useState(false);
 
-  return (
-    <div className="base-card login-card">
-      <h1>LOGIN</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-4">
-          <input
-            {...register('username')}
-            type="text"
-            className="form-control base-input"
-            placeholder="Email"
-            name="username"
-          />
+    const { register, handleSubmit } = useForm<FormData>();
+
+    const onSubmit = (formData: FormData) => {
+        requestBackendLogin(formData)
+            .then((response) => {
+                setHasError(false);
+                console.log('SUCESSO', response);
+            })
+            .catch((error) => {
+                setHasError(true);
+                console.log('ERRO', error);
+            });
+    };
+
+    return (
+        <div className="base-card login-card">
+            <h1>LOGIN</h1>
+            {hasError && (
+                <div className="alert alert-danger">
+                   Erro ao tentar efetuar o login
+                </div>
+            )}
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="mb-4">
+                    <input
+                        {...register('username')}
+                        type="text"
+                        className="form-control base-input"
+                        placeholder="Email"
+                        name="username"
+                    />
+                </div>
+                <div className="mb-2">
+                    <input
+                        {...register('password')}
+                        type="password"
+                        className="form-control base-input "
+                        placeholder="Password"
+                        name="password"
+                    />
+                </div>
+                <Link to="/admin/auth/recover" className="login-link-recover">
+                    Esqueci a senha
+                </Link>
+                <div className="login-submit">
+                    <ButtonIcon text="Fazer login" />
+                </div>
+                <div className="signup-container">
+                    <span className="not-registered">Não tem Cadastro?</span>
+                    <Link to="/admin/auth/register" className="login-link-register">
+                        CADASTRAR
+                    </Link>
+                </div>
+            </form>
         </div>
-        <div className="mb-2">
-          <input
-            {...register('password')}
-            type="password"
-            className="form-control base-input "
-            placeholder="Password"
-            name="password"
-          />
-        </div>
-        <Link to="/admin/auth/recover" className="login-link-recover">
-          Esqueci a senha
-        </Link>
-        <div className="login-submit">
-          <ButtonIcon text="Fazer login" />
-        </div>
-        <div className="signup-container">
-          <span className="not-registered">Não tem Cadastro?</span>
-          <Link to="/admin/auth/register" className="login-link-register">
-            CADASTRAR
-          </Link>
-        </div>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default Login;
