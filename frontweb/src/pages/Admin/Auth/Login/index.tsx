cc1209ebe460ea6ebd24e89867a2518393737ea4
Gabriel Tavares <tavaresgabriel963@gmail.com>
2021-10-26 22:25:30 -0400

    Saving auth data to localStorage

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from 'react-hook-form';
-import { requestBackendLogin } from 'util/requests';
+import { getAuthData, requestBackendLogin, saveAuthData } from 'util/requests';
 import { useState } from 'react';
 
 import './styles.css';
@@ -20,6 +20,9 @@ const Login = () => {
     const onSubmit = (formData: FormData) => {
         requestBackendLogin(formData)
             .then((response) => {
+                saveAuthData(response.data);
+                const token = getAuthData().access_token;
+                console.log('TOKEN GERADO:' + token);
                 setHasError(false);
                 console.log('SUCESSO', response);
             })
