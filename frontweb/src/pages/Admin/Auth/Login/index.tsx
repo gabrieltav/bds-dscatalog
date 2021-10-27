bde10a9caa7f01067d42ecbb7f7b9fab821119f4
Gabriel Tavares <tavaresgabriel963@gmail.com>
2021-10-26 21:42:47 -0400

    Form validation

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -15,7 +15,7 @@ const Login = () => {
 
     const [hasError, setHasError] = useState(false);
 
-    const { register, handleSubmit } = useForm<FormData>();
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
 
     const onSubmit = (formData: FormData) => {
         requestBackendLogin(formData)
@@ -34,27 +34,37 @@ const Login = () => {
             <h1>LOGIN</h1>
             {hasError && (
                 <div className="alert alert-danger">
-                   Erro ao tentar efetuar o login
+                    Erro ao tentar efetuar o login
                 </div>
             )}
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="mb-4">
                     <input
-                        {...register('username')}
+                        {...register('username', {
+                            required: 'Campo obrigatório',
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: 'Email inválido'
+                            }
+                        })}
                         type="text"
                         className="form-control base-input"
                         placeholder="Email"
                         name="username"
                     />
+                    <div className="invalid-feedback d-block">{errors.username?.message}</div>
                 </div>
                 <div className="mb-2">
                     <input
-                        {...register('password')}
+                        {...register('password', {
+                            required: 'Campo obrigatório'
+                        })}
                         type="password"
                         className="form-control base-input "
                         placeholder="Password"
                         name="password"
                     />
+                    <div className="invalid-feedback d-block">{errors.password?.message}</div>
                 </div>
                 <Link to="/admin/auth/recover" className="login-link-recover">
                     Esqueci a senha
