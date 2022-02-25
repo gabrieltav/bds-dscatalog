0e4b53856587bb5cf025540ab182a16a23244ce4
Gabriel Tavares <tavaresgabriel963@gmail.com>
2022-02-25 16:17:40 -0400

    Redirecting to intended route after login

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -1,96 +1,113 @@
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from 'react-hook-form';
-import {getTokenData, requestBackendLogin, saveAuthData } from 'util/requests';
+import { getTokenData, requestBackendLogin, saveAuthData } from 'util/requests';
 import { useContext, useState } from 'react';
-
-import './styles.css';
 import { AuthContext } from 'AuthContext';
 
+import './styles.css';
+
 type FormData = {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
+};
+
+type LocationState = {
+  from: string;
 };
 
 const Login = () => {
+  const location = useLocation<LocationState>();
 
-    const { setAuthContextData } = useContext(AuthContext);
+  const { from } = location.state || { from: { pathname: '/admin' } };
 
-    const [hasError, setHasError] = useState(false);
+  const { setAuthContextData } = useContext(AuthContext);
 
-    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+  const [hasError, setHasError] = useState(false);
 
-    const history = useHistory();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
-    const onSubmit = (formData: FormData) => {
-        requestBackendLogin(formData)
-            .then((response) => {
-                saveAuthData(response.data);
-                setHasError(false);
-                setAuthContextData({
-                    authenticated: true,
-                    tokenData: getTokenData(),
-                  })
-                history.push('/admin');
-            })
-            .catch((error) => {
-                setHasError(true);
-                console.log('ERRO', error);
-            });
-    };
+  const history = useHistory();
 
-    return (
-        <div className="base-card login-card">
-            <h1>LOGIN</h1>
-            {hasError && (
-                <div className="alert alert-danger">
-                    Erro ao tentar efetuar o login
-                </div>
-            )}
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="mb-4">
-                    <input
-                        {...register('username', {
-                            required: 'Campo obrigatório',
-                            pattern: {
-                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                                message: 'Email inválido'
-                            }
-                        })}
-                        type="text"
-                        className={`form-control base-input ${errors.username ? 'is-invalid' : ''}`}
-                        placeholder="Email"
-                        name="username"
-                    />
-                    <div className="invalid-feedback d-block">{errors.username?.message}</div>
-                </div>
-                <div className="mb-2">
-                    <input
-                        {...register('password', {
-                            required: 'Campo obrigatório'
-                        })}
-                        type="password"
-                        className={`form-control base-input ${errors.password ? 'is-invalid' : ''}`}
-                        placeholder="Password"
-                        name="password"
-                    />
-                    <div className="invalid-feedback d-block">{errors.password?.message}</div>
-                </div>
-                <Link to="/admin/auth/recover" className="login-link-recover">
-                    Esqueci a senha
-                </Link>
-                <div className="login-submit">
-                    <ButtonIcon text="Fazer login" />
-                </div>
-                <div className="signup-container">
-                    <span className="not-registered">Não tem Cadastro?</span>
-                    <Link to="/admin/auth/register" className="login-link-register">
-                        CADASTRAR
-                    </Link>
-                </div>
-            </form>
+  const onSubmit = (formData: FormData) => {
+    requestBackendLogin(formData)
+      .then((response) => {
+        saveAuthData(response.data);
+        setHasError(false);
+        setAuthContextData({
+          authenticated: true,
+          tokenData: getTokenData(),
+        });
+        history.replace(from);
+      })
+      .catch((error) => {
+        setHasError(true);
+        console.log('ERRO', error);
+      });
+  };
+
+  return (
+    <div className="base-card login-card">
+      <h1>LOGIN</h1>
+      {hasError && (
+        <div className="alert alert-danger">Erro ao tentar efetuar o login</div>
+      )}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-4">
+          <input
+            {...register('username', {
+              required: 'Campo obrigatório',
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'Email inválido',
+              },
+            })}
+            type="text"
+            className={`form-control base-input ${
+              errors.username ? 'is-invalid' : ''
+            }`}
+            placeholder="Email"
+            name="username"
+          />
+          <div className="invalid-feedback d-block">
+            {errors.username?.message}
+          </div>
         </div>
-    );
+        <div className="mb-2">
+          <input
+            {...register('password', {
+              required: 'Campo obrigatório',
+            })}
+            type="password"
+            className={`form-control base-input ${
+              errors.password ? 'is-invalid' : ''
+            }`}
+            placeholder="Password"
+            name="password"
+          />
+          <div className="invalid-feedback d-block">
+            {errors.password?.message}
+          </div>
+        </div>
+        <Link to="/admin/auth/recover" className="login-link-recover">
+          Esqueci a senha
+        </Link>
+        <div className="login-submit">
+          <ButtonIcon text="Fazer login" />
+        </div>
+        <div className="signup-container">
+          <span className="not-registered">Não tem Cadastro?</span>
+          <Link to="/admin/auth/register" className="login-link-register">
+            CADASTRAR
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
