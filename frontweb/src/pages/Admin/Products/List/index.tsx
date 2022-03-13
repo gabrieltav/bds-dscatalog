27ba4ecfa4cf530d02d1ef989e576da0485e3fdb
Gabriel Tavares <tavaresgabriel963@gmail.com>
2022-03-12 21:31:03 -0400

    CategoryBadge

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -1,4 +1,4 @@
-import ProductCrudCard from 'components/ProductCrudCard';
+import ProductCrudCard from 'pages/Admin/Products/ProductCrudCard';
 import { Link } from 'react-router-dom';
 import './styles.css';
 
@@ -16,6 +16,10 @@ const List = () => {
       {
         id: 2,
         name: 'Eletrônicos',
+      },
+      {
+        id: 1,
+        name: 'Computadores',
       },
     ],
   };
